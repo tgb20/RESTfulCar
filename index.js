08ed1b166e127ec67cb87b76b4914be578471606
Jacob Bashista <tgb.bashista@gmail.com>
2021-04-17 15:51:35 -0400

    Use env port and repeat queries

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = 5000;
+const port = process.env.PORT;
 
 
 let batterySOC = 0;
@@ -10,7 +10,7 @@ let chargeLevel = 0;
 
 getNewestData();
 
-setTimeout(() => {
+setInterval(() => {
   getNewestData();
 }, 600000);
 
