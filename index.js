0e50f28392870e729db5da6c0883f79baae4e889
Jacob Bashista <tgb.bashista@gmail.com>
2021-04-17 08:59:07 -0400

    Switched to .env, split Lametric and raw data

--- index.js
+++ index.js
@@ -1,55 +1,67 @@
-const config = require("./config.json");
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
-
-
 const app = express();
-const port = config.port;
-
-app.get('/charge', (req, res) => {
-
-  axios.post('https://api.autopi.io/auth/login/', {
-    username: config.autopi_username,
-    email: config.autopi_email,
-    password: config.autopi_password
-  })
-    .then((response) => {
-
-      let token = response.data.token;
-
-      let currentTime = new Date(Date.now() - 3600000 * 10).toISOString();
+const port = 5000;
 
 
-      axios.get(`https://api.autopi.io/logbook/storage/read/?device_id=fbb8ceb7-f437-4755-9d27-489e17d628e8&field=obd.batt-soc.value&field_type=long&from_utc=${currentTime}`, {
-        headers: {
-          'Authorization': `bearer ${token}`
+let batterySOC = 0;
+let chargeLevel = 0;
+
+getNewestData();
+
+setTimeout(() => {
+  getNewestData();
+}, 600000);
+
+
+app.get('/lametric', (req, res) => {
+  res.json(
+    {
+      'frames': [
+        {
+          'text': `${batterySOC}%`,
+          'icon': 'i39605',
+          'index': 0
         }
-      }).then((response) => {
-        let charge = response.data[response.data.length - 1].value;
-        res.json(
-          {
-            'frames': [
-              {
-                'text': `${charge}%`,
-                'icon': 'i39605',
-                'index': 0
-              }
-            ]
-          }
-        );
-
-      }).catch((error) => {
-        console.log(error);
-        res.json({ error: error });
-      });
-    })
-    .catch((error) => {
-      console.log(error);
-      res.json({ error: error });
-    });
-
-
+      ]
+    }
+  );
 });
+
+app.get('/data', (req, res) => {
+  res.json(
+    {
+      'batterySOC': batterySOC,
+      'chargeLevel': chargeLevel
+    }
+  );
+});
+
+function getNewestData() {
+
+  let oneWeekAgo = new Date(Date.now() - 6.048e+8).toISOString();
+
+  axios.get(`https://api.autopi.io/logbook/storage/read/?device_id=fbb8ceb7-f437-4755-9d27-489e17d628e8&field=obd.batt-soc.value&field_type=long&from_utc=${oneWeekAgo}`, {
+    headers: {
+      'Authorization': `APIToken ${process.env.API_KEY}`
+    }
+  }).then((response) => {
+    batterySOC = response.data[response.data.length - 1].value;
+  }).catch((error) => {
+    console.log(error);
+  });
+
+  axios.get(`https://api.autopi.io/logbook/storage/read/?device_id=fbb8ceb7-f437-4755-9d27-489e17d628e8&field=obd.charger-level.value&field_type=long&from_utc=${oneWeekAgo}`, {
+    headers: {
+      'Authorization': `APIToken ${process.env.API_KEY}`
+    }
+  }).then((response) => {
+    chargeLevel = response.data[response.data.length - 1].value;
+  }).catch((error) => {
+    console.log(error);
+  });
+}
 
 app.listen(port, () => {
   console.log(`RESTfulCar listening on port: ${port}`);
